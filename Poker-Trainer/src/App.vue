8ae8a9867e758547eaf10e4131a107308dc9c01a
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-01 20:45:52 +0100

    first commit

--- Poker-Trainer/src/App.vue
+++ Poker-Trainer/src/App.vue
@@ -1,54 +1,16 @@
 <template>
-<v-app>    
-     <v-navigation-drawer permanent v-model="drawer" app>
-        <v-list>
-          <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
-            <v-list-item-content>
-              <v-list-item-title class="title">
-                {{item.title}}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-app-bar app flat> 
-        <v-toolbar-title> Poker </v-toolbar-title>
-      </v-app-bar>
-      <v-main>
-         <router-view>  
-         </router-view>
-     </v-main>
-</v-app>
-
+  <Navbar />
 </template>
+
 <script>
-
-
+import Navbar from "./components/Navbar";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
+    Navbar,
   },
-  data: () => ({
-    drawer: null,
-    items: [{
-      title: "Home Page",
-      to: "/home"
-    },
-    {
-      title: "Acount Page",
-      to: "/account"
-    },
-    {
-      title: "Scenario",
-      to: "/scenario"
-    },
-    {
-      title: "Leaderboard",
-      to: "/leaderboard"
-    },
-    ]
-  }),
+  data: () => ({}),
 };
 </script>
+
+<style scoped></style>
---
+++ Poker-Trainer/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-navigation-drawer dark permanent v-model="drawer" app>
+      <v-list>
+        <v-list-item>
+          <img id="logo" src="..\assets\logo.jpg" alt="img not found" />
+          <h1>TCD Poker Trainer</h1>
+        </v-list-item>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view> </router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  components: {},
+  data: () => ({
+    drawer: null,
+    items: [
+      {
+        title: "Home ",
+        to: "/home",
+      },
+      {
+        title: "Profile",
+        to: "/account",
+      },
+      {
+        title: "Scenario",
+        to: "/scenario",
+      },
+      {
+        title: "Leaderboard",
+        to: "/leaderboard",
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+#logo {
+  width: 50px;
+}
+</style>
