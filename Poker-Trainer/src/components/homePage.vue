123005ad5875762f9538e9a1b544679bef5d4c83
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-08 17:03:56 +0100

    progress on home page styling

--- Poker-Trainer/src/components/homePage.vue
+++ Poker-Trainer/src/components/homePage.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="main-div">
-    <h2>Want to be the best at the table?</h2>
-    <div>
-      <router-link v-if="!signedIn" to="/account" class="signup">
-        Sign Up
-      </router-link>
-      <router-link v-if="signedIn" to="/scenario" class="signup">
-        Practice
-      </router-link>
+    <div id="home-banner">
+      <img id="home-banner-background" src="..\assets\home-banner.png" alt="" />
+
+      <div id="home-banner-foreground">
+        <h2>Want to be the best at the table?</h2>
+        <router-link v-if="!signedIn" to="/account" class="signup">
+          Sign Up
+        </router-link>
+        <router-link v-if="signedIn" to="/scenario" class="signup">
+          Practice
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -36,9 +40,30 @@ export default {
 .signup {
   color: red;
 }
-.test {
-  width: 100px;
-  height: 100px;
-  background-color: blanchedalmond;
+#home-banner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#home-banner-background {
+  opacity: 45%;
+  position: static;
+  width: 100%;
+}
+
+#home-banner-foreground {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-size: 3vw;
+}
+#home-banner-foreground h2 {
+  margin-bottom: 10%;
 }
 </style>
