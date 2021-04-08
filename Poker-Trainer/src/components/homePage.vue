8228dbb570fc623d36b683e4c754b4a7545bedbe
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-08 12:53:13 +0100

    removed unnecessary nested v-app tags (replaced with divs for now)

--- Poker-Trainer/src/components/homePage.vue
+++ Poker-Trainer/src/components/homePage.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div id="main-div">
     <h2>Want to be the best at the table?</h2>
     <div>
-      <router-link to="/account" class="signup">Sign Up</router-link>
+      <router-link v-if="!signedIn" to="/account" class="signup">
+        Sign Up
+      </router-link>
+      <router-link v-if="signedIn" to="/scenario" class="signup">
+        Practice
+      </router-link>
     </div>
   </div>
 </template>
@@ -10,14 +15,30 @@
 <script>
 export default {
   name: "homePage",
-  componets: {},
-  data: () => ({}),
-  methods: {},
+  data: () => ({
+    signedIn: true,
+    drawer: false,
+  }),
 };
 </script>
 
 <style scoped>
+#main-div {
+  padding: 0%;
+  margin: 0%;
+  min-height: 200%;
+  background-color: rgb(22, 22, 22);
+  color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .signup {
   color: red;
+}
+.test {
+  width: 100px;
+  height: 100px;
+  background-color: blanchedalmond;
 }
 </style>
