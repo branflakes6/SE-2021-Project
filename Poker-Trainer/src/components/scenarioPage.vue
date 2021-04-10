96ec0df45d200e8b1b6386a85257f1b69e3c12a4
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-10 17:32:21 +0100

    moved arround the cards and chip pits to better fit texas holdem' layout

--- Poker-Trainer/src/components/scenarioPage.vue
+++ Poker-Trainer/src/components/scenarioPage.vue
@@ -29,6 +29,7 @@
           <!-- opponent 4 cards -->
           <div id="opponent-4" class="player-container" :key="flip">
             <template v-if="scenarioParams.numOfOpponents >= 4">
+              <div class="player-chips-container"></div>
               <div class="player-cards">
                 <vue-playing-card
                   :signature="scenarioParams.opponentFourParams.cardOne"
@@ -39,70 +40,74 @@
                   :width="cardWidth"
                 ></vue-playing-card>
               </div>
-              <div class="player-chips-container"></div>
             </template>
           </div>
         </div>
 
         <div class="group-b">
-          <!-- opponent 1 cards -->
-          <div id="opponent-1" class="player-container" :key="flip">
-            <template v-if="scenarioParams.numOfOpponents >= 1">
+          <div class="group-d">
+            <div id="pot" class="player-chips-container"></div>
+
+            <!-- the "community" cards -->
+            <div class="community-cards-container">
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.firstCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.secondCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.thirdCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.fourthCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.fifthCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+              <vue-playing-card
+                :signature="scenarioParams.cardsOnTable.sixthCard"
+                :width="cardWidth"
+              ></vue-playing-card>
+            </div>
+          </div>
+
+          <div class="group-e">
+            <!-- opponent 1 cards -->
+            <div id="opponent-1" class="player-container" :key="flip">
+              <template v-if="scenarioParams.numOfOpponents >= 1">
+                <div class="player-chips-container"></div>
+                <div class="player-cards">
+                  <vue-playing-card
+                    :signature="scenarioParams.opponentOneParams.cardOne"
+                    :width="cardWidth"
+                  ></vue-playing-card>
+                  <vue-playing-card
+                    :signature="scenarioParams.opponentOneParams.cardTwo"
+                    :width="cardWidth"
+                  ></vue-playing-card>
+                </div>
+              </template>
+              <!-- users cards and options -->
+            </div>
+            <div class="user-container">
+              <!-- users cards -->
               <div class="player-chips-container"></div>
-              <div class="player-cards">
+              <div class="user-cards">
                 <vue-playing-card
-                  :signature="scenarioParams.opponentOneParams.cardOne"
+                  :signature="scenarioParams.userParams.cardOne"
                   :width="cardWidth"
                 ></vue-playing-card>
                 <vue-playing-card
-                  :signature="scenarioParams.opponentOneParams.cardTwo"
+                  :signature="scenarioParams.userParams.cardTwo"
                   :width="cardWidth"
                 ></vue-playing-card>
               </div>
-            </template>
-          </div>
-          <div class="player-chips-container"></div>
-
-          <!-- the "community" cards -->
-          <div class="community-cards-container">
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.firstCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.secondCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.thirdCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.fourthCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.fifthCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-            <vue-playing-card
-              :signature="scenarioParams.cardsOnTable.sixthCard"
-              :width="cardWidth"
-            ></vue-playing-card>
-          </div>
-          <!-- users cards and options -->
-          <div class="user-container">
-            <!-- users cards -->
-            <div class="player-chips-container"></div>
-            <div class="user-cards">
-              <vue-playing-card
-                :signature="scenarioParams.userParams.cardOne"
-                :width="cardWidth"
-              ></vue-playing-card>
-              <vue-playing-card
-                :signature="scenarioParams.userParams.cardTwo"
-                :width="cardWidth"
-              ></vue-playing-card>
             </div>
           </div>
         </div>
@@ -110,7 +115,6 @@
           <!-- opponent 3 cards -->
           <div id="opponent-3" class="player-container" :key="flip">
             <template v-if="scenarioParams.numOfOpponents >= 3">
-              <div class="player-chips-container"></div>
               <div class="player-cards">
                 <vue-playing-card
                   :signature="scenarioParams.opponentThreeParams.cardOne"
@@ -121,6 +125,7 @@
                   :width="cardWidth"
                 ></vue-playing-card>
               </div>
+              <div class="player-chips-container"></div>
             </template>
           </div>
 
@@ -358,6 +363,21 @@ export default {
   justify-content: space-between;
   align-content: center;
 }
+
+.group-d {
+  /*border: solid white;*/
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+.group-e {
+  /*border: solid white;*/
+  height: 50%;
+  display: flex;
+  align-items: flex-end;
+}
 .community-cards-container {
   /*border: solid white;*/
   display: flex;
@@ -366,11 +386,10 @@ export default {
   height: 120px;
 }
 .player-container {
-  /*border: solid white;*/
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 120px;
 }
 .player-cards {
   display: flex;
@@ -382,39 +401,43 @@ export default {
   border-radius: 40%;
   width: 100px;
   height: 100px;
-  margin: 0 10px 0 10px;
+  margin: 10px;
+}
+#pot {
+  border: solid gold;
 }
 #opponent-1 {
   /*border: solid white;*/
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  padding: 10px 0 10px 0;
+  margin: 0 10px 0 10px;
 }
 #opponent-2 {
-  /*border: solid white;*/
-  margin: 100px 0 0 0px;
+  margin: 50px 0 0 0px;
   transform: rotate(-45deg);
   display: flex;
   justify-content: flex-end;
   padding: 0 40px 0 0;
 }
 #opponent-3 {
-  /*border: solid white;*/
-  margin: 100px 0 0 0;
+  margin: 50px 0 0 0;
   transform: rotate(45deg);
   display: flex;
   justify-content: flex-start;
   padding: 0 0 0 40px;
 }
 #opponent-4 {
-  /* /*border: solid white; */
-  margin: 0 0 100px 50px;
+  margin: 0 0 30px 50px;
   transform: rotate(45deg);
   display: flex;
   justify-content: flex-end;
   padding: 0 40px 0 0;
 }
 #opponent-5 {
-  /*border: solid white;*/
-  margin: 0 50px 100px 0;
+  margin: 0 50px 30px 0;
   transform: rotate(-45deg);
   display: flex;
   justify-content: flex-start;
@@ -423,9 +446,12 @@ export default {
 .user-container {
   /*border: solid white;*/
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  height: 120px;
+  height: 100%;
+  padding: 10px 0 10px 0;
+  margin: 0 10px 0 10px;
 }
 .user-cards {
   display: flex;
@@ -476,7 +502,7 @@ export default {
     padding: 0 0 0 40px;
   }
   #opponent-4 {
-    /* /*border: solid white; */
+    /*border: solid white;*/
     margin: 0 0 0 0;
     padding: 0 40px 20px 0;
   }
