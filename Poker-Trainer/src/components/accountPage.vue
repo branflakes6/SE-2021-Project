67b841482ec39a7a64d438ca31b97aa9bcefdf1c
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-12 23:51:41 +0100

    progress on account page styling

--- Poker-Trainer/src/components/accountPage.vue
+++ Poker-Trainer/src/components/accountPage.vue
@@ -1,20 +1,53 @@
 <template>
-  <div>
-    <div>
-      <h1>Welcome to the Account Page</h1>
-      <div>
-        <v-btn depressed to="/loginPage"> Login </v-btn>
-        <v-btn depressed to="/createAccount"> Create Account </v-btn>
-      </div>
-    </div>
+  <div id="main-div">
+    <h1>Welcome to the Account Page</h1>
+
+    <v-btn
+      width="200"
+      outlined
+      v-if="!signedIn"
+      class="signup"
+      v-on:click="showSignUpForm = !showSignUpForm"
+      color="red"
+    >
+      Create Account
+    </v-btn>
+    <v-btn
+      width="200"
+      outlined
+      v-if="!signedIn"
+      class="signup"
+      v-on:click="showSignInForm = !showSignInForm"
+      color="red"
+    >
+      Sign In
+    </v-btn>
+    <signUpForm />
   </div>
 </template>
 
 <script>
+import signUpForm from "./signUpForm";
 export default {
   name: "accountPage",
-  componets: {},
-  data: () => ({}),
+  componets: {
+    signUpForm,
+  },
+  data: () => ({
+    showSignUpForm: false,
+    showSignInForm: false,
+  }),
   methods: {},
 };
 </script>
+
+<style scoped>
+#main-div {
+  color: white;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
