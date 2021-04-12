57ba2bc612f0fd1995bcdf071d02b31aab249cca
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-12 23:11:58 +0100

    made a signup form

---
+++ Poker-Trainer/src/components/signUpForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card dark id="signup-form">
+    <v-card-title>
+      <span class="headline">Sign up</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="Email" required></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="Username" required></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              label="Password"
+              :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPass ? 'text' : 'password'"
+              required
+              @click:append="showPass = !showPass"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="red darken-1" text @click="dialog = false">
+        Create Account
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "signUpForm",
+  data: () => ({
+    showPass: false,
+  }),
+};
+</script>
+
+<style scoped>
+#signup-form {
+  color: white;
+  text-align: center;
+}
+</style>
