71f7130e8bd35440137edbab120069383db4dbcc
Brian Farrell <55883565+branflakes6@users.noreply.github.com>
2021-04-15 17:39:00 +0100

    Update firebase.js

--- Poker-Trainer/src/firebase.js
+++ Poker-Trainer/src/firebase.js
@@ -4,15 +4,12 @@ import "firebase/auth";
 
 
 const firebaseConfig = {
-    //apiKey: process.env.VUE_FIREBASE_API_KEY,
-    apiKey: "AIzaSyAfhrnhESJEvgVtDj1WwjKKP1Fh2wspqLM",
+    apiKey: process.env.VUE_FIREBASE_API_KEY,
     authDomain: "poker-trainer-d2a00.firebaseapp.com",
     projectId: "poker-trainer-d2a00",
     storageBucket: "poker-trainer-d2a00.appspot.com",
-    //messagingSenderId: process.env.VUE_FIREBASE_MESSAGEID,
-    messagingSenderId: "604223404719",
-    //appId: process.env.VUE_FIREBASE_APPID
-    appId : "1:604223404719:web:a18a4714b452854b23725a"
+    messagingSenderId: process.env.VUE_FIREBASE_MESSAGEID,
+    appId: process.env.VUE_FIREBASE_APPID
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
