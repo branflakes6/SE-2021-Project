f82d7bbde7b00ff2081adefdad740b04c972b267
Brian Farrell <55883565+branflakes6@users.noreply.github.com>
2021-04-15 17:37:53 +0100

    Update routes.js

--- Poker-Trainer/src/routes/routes.js
+++ Poker-Trainer/src/routes/routes.js
@@ -24,6 +24,49 @@ let router = new Router({
       meta: {},
     },
     {
+      path: "/loginPage",
+      name: "loginPage",
+      component: () =>
+        import(/*webpackChunkName: "account"*/ "..//components//loginPage.vue"),
+      meta: {},
+    },
+    {
+      path: "/createAccount",
+      name: "createAccount",
+      component: () =>
+        import(
+          /*webpackChunkName: "account"*/ "..//components//createAccount.vue"
+        ),
+      meta: {},
+    },
+    {
+      path: "/scenarioBrowser",
+      name: "scenarioBrowser",
+      component: () =>
+        import(
+          /*webpackChunkName: "scenario"*/ "..//components//scenarioBrowser.vue"
+        ),
+      meta: {},
+    },
+    {
+      path: "/scenarioPage/:scenarioID",
+      name: "scenarioPage",
+      component: () =>
+        import(
+          /*webpackChunkName: "scenario"*/ "..//components//scenarioPage.vue"
+        ),
+      meta: {},
+    },
+    {
+      path: "/leaderboard",
+      name: "leaderboardPage",
+      component: () =>
+        import(
+          /*webpackChunkName: "leaderboard"*/ "..//components//leaderboard.vue"
+        ),
+      meta: {},
+    },
+    {
       path: "/fakeLeaderboard",
       name: "fakeLeaderboardPage",
       component: () =>
