41983e74f4758e9ee6baf73ea6151eb3394e0332
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-18 16:59:34 +0100

    starter template for player component

---
+++ Poker-Trainer/src/components/player.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="player-container">
+    <!-- users cards -->
+    <div class="d-c-container">
+      <div v-if="playerParams.dealer" class="dealer-chip-container">
+        <img
+          class="dealer-chip"
+          src="../assets/dealer-chip.png"
+          alt="dealer-chip"
+        />
+      </div>
+      <div class="player-chips-container">
+        Chips: {{ playerParams.chipsAvailable }}
+      </div>
+    </div>
+    <div class="player-cards">
+      <vue-playing-card
+        :signature="playerParams.cardOne"
+        :width="cardWidth"
+      ></vue-playing-card>
+      <vue-playing-card
+        :signature="playerParams.cardTwo"
+        :width="cardWidth"
+      ></vue-playing-card>
+    </div>
+    <v-btn class="player-details">
+      {{ playerParams.name }}
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "player",
+  props: ["playerParams", "cardWidth"],
+};
+</script>
