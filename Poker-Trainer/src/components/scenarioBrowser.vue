e7dd0eb6e2415c71337fa3f5ff23a296b7aeefee
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-22 13:33:09 +0100

    formatting changes

--- Poker-Trainer/src/components/scenarioBrowser.vue
+++ Poker-Trainer/src/components/scenarioBrowser.vue
@@ -2,102 +2,112 @@
 <template>
   <div id="main-div">
     <div>
-      <creationForm/>
-   </div>
+      <creationForm />
+    </div>
 
-<div>
-  <v-container>
-        <v-row align="center" justify = "center">
-            <h1 align="center" justify = "center"> Choose a category</h1>
-            <div >
-            <v-select 
-            text-align="center"
-            item-text="name"
-            item-value="ID"
-            v-model="selectedCat"
-            :items="categories"
+    <div>
+      <v-container>
+        <v-row align="center" justify="center">
+          <h1 align="center" justify="center">Choose a category</h1>
+          <div>
+            <v-select
+              text-align="center"
+              item-text="name"
+              item-value="ID"
+              v-model="selectedCat"
+              :items="categories"
             >
             </v-select>
-            </div>       
+          </div>
           <v-btn v-on:click="updateCategory">Update</v-btn>
         </v-row>
       </v-container>
-</div>
+    </div>
 
-  <v-layout wrap justify-center>
-      <div v-for="item in masterList" :key="item" id="scenarioThumbnail-container">
-              <scenarioThumbnail
-              v-bind:scenarioID="item.id"
-              v-bind:title="item.name"
-              v-bind:author="item.author"
-              v-bind:description="item.description"
-              />
+    <v-layout wrap justify-center>
+      <div
+        v-for="item in masterList"
+        :key="item"
+        id="scenarioThumbnail-container"
+      >
+        <scenarioThumbnail
+          v-bind:scenarioID="item.id"
+          v-bind:title="item.name"
+          v-bind:author="item.author"
+          v-bind:description="item.description"
+        />
       </div>
-  </v-layout>
-
-  
+    </v-layout>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase/app'
+import firebase from "firebase/app";
 const db = firebase.firestore();
 import scenarioThumbnail from "./scenarioThumbnail";
-import creationForm from './creationForm.vue';
+import creationForm from "./creationForm.vue";
 export default {
   name: "scenarioBrowser",
   components: {
     scenarioThumbnail,
     creationForm,
   },
- methods: {
-   search (){
-     this.searchTerm = this.masterList_id
-   },
-   updateCategory(){
-     this.masterList = []
-     db.collection('scenarios').doc("masterList").get().then(doc => {
-     let docs = doc.data()
-     console.log(docs)
-     console.log(docs.Scenarios)
-    for(let index of docs.Scenarios){
-      console.log(index)
-      if (index.category == this.selectedCat || this.selectedCat == "All"){
-        this.masterList.push(index)
-      }
-    }
-    })
-   },
- },
+  methods: {
+    search() {
+      this.searchTerm = this.masterList_id;
+    },
+    updateCategory() {
+      this.masterList = [];
+      db.collection("scenarios")
+        .doc("masterList")
+        .get()
+        .then((doc) => {
+          let docs = doc.data();
+          console.log(docs);
+          console.log(docs.Scenarios);
+          for (let index of docs.Scenarios) {
+            console.log(index);
+            if (
+              index.category == this.selectedCat ||
+              this.selectedCat == "All"
+            ) {
+              this.masterList.push(index);
+            }
+          }
+        });
+    },
+  },
   computed: {
     gridStyle() {
       return {
-        gridTemplateColumns: `repeat(${this.numberOfColumns}, minmax(100px, 1fr))`
-      }
+        gridTemplateColumns: `repeat(${this.numberOfColumns}, minmax(100px, 1fr))`,
+      };
     },
   },
- data () {
+  data() {
     return {
-    masterList: [],
-    categories: ["All","Open","ISO","3Bet","4Bet","5Bet"],
-    selectedCat:"All",
-    masterList_id: "",
-    newID: "",
-    searchTerm: "",
-    loaded: false,
-    select: "",
-    showForm: false,
-    // card values
-   }
+      masterList: [],
+      categories: ["All", "Open", "ISO", "3Bet", "4Bet", "5Bet"],
+      selectedCat: "All",
+      masterList_id: "",
+      newID: "",
+      searchTerm: "",
+      loaded: false,
+      select: "",
+      showForm: false,
+      // card values
+    };
   },
 
- created() {
-    db.collection('scenarios').doc("masterList").get().then(doc => {
-      this.masterList = doc.data().Scenarios
-      this.loaded = true
-    })
+  created() {
+    db.collection("scenarios")
+      .doc("masterList")
+      .get()
+      .then((doc) => {
+        this.masterList = doc.data().Scenarios;
+        this.loaded = true;
+      });
   },
-
 };
 </script>
 
@@ -113,7 +123,7 @@ export default {
   justify-content: center;
   margin: 0 auto;
   padding: 25px;
-  background-color: rgb(22, 22, 22);
+  background-color: #2e2e2e;
 }
 @media screen and (max-width: 1250px) {
   #scenarioThumbnail-container {
