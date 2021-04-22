bbabbbd0a907075aea6d6b9fd5be3ef176712ddb
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-22 19:00:20 +0100

    styled the player details somewhat

--- Poker-Trainer/src/components/playerDetails.vue
+++ Poker-Trainer/src/components/playerDetails.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>{{ playerDetails }}</h1>
-  </div>
+  <v-card>
+    <div id="details-container">
+      <h1>{{ playerDetails.name }}</h1>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -10,3 +12,15 @@ export default {
   props: ["playerDetails"],
 };
 </script>
+
+<style scoped>
+#details-container {
+  border: solid white;
+  color: white;
+  background-color: #1111 !important;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
