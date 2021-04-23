8d632ee5c92018277274ca16da499807878b4ae5
Brian Farrell <farrelb6@tcd.ie>
2021-04-23 13:34:41 +0100

    changed turns to actions

--- Poker-Trainer/src/components/playerDetails.vue
+++ Poker-Trainer/src/components/playerDetails.vue
@@ -5,7 +5,7 @@
       <h2>Attributes:</h2>
       <h2>{{ playerDetails.attributes }}</h2>
       <div id="spacer"></div>
-      <h1>Previous Turns</h1>
+      <h1>Previous Actions</h1>
       <h4>{{ playerDetails.prevMove }}</h4>
     </div>
   </v-card>
