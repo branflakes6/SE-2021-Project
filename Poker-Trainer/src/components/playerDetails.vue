38af22ae8932d3fa06b876aa98e3ddbb801ac082
Cornel Jonathan Cicai <jccicai@yahoo.ie>
2021-04-23 12:31:39 +0100

    styled player details somewhat

--- Poker-Trainer/src/components/playerDetails.vue
+++ Poker-Trainer/src/components/playerDetails.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card>
     <div id="details-container">
-      <h1>{{playerDetails.name}}</h1>
-      <h2> Attributes: </h2>
-      <h2 >{{playerDetails.attributes}}</h2>
-      <spacer></spacer>
-      <h1 >Previous Turns</h1>
-      <h4>{{playerDetails.prevMove}}</h4>
+      <h1>{{ playerDetails.name }}</h1>
+      <h2>Attributes:</h2>
+      <h2>{{ playerDetails.attributes }}</h2>
+      <div id="spacer"></div>
+      <h1>Previous Turns</h1>
+      <h4>{{ playerDetails.prevMove }}</h4>
     </div>
   </v-card>
 </template>
@@ -22,10 +22,12 @@ export default {
 #details-container {
   border: solid white;
   color: white;
-  background-color: #1111 !important;
+  background-color: #1111;
   min-height: 50vh;
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   text-align: center;
 }
+</style>
